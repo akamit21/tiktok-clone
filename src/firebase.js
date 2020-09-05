0550af42624783bde044577d2a59228802d0ee6c
Amit Kumar <akamit21@outlook.com>
2020-09-06 03:18:05 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDFdRC-bD4V_e_g2-TCeqMqqGyOUi9aFR0",
+    authDomain: "tiktok-clone-e5966.firebaseapp.com",
+    databaseURL: "https://tiktok-clone-e5966.firebaseio.com",
+    projectId: "tiktok-clone-e5966",
+    storageBucket: "tiktok-clone-e5966.appspot.com",
+    messagingSenderId: "995228053940",
+    appId: "1:995228053940:web:b823a0e066dabc38cb42e1",
+    measurementId: "G-TG6E7WD546"
+};
